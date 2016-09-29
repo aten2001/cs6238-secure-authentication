e5f923090c05f35ab6ffab2592a3e961d0044156
Edmonds <brianedmonds90@gmail.com>
2016-09-29 18:56:40 -0400

    parser.py should parse the input correctly now.

--- parser.py
+++ parser.py
@@ -3,13 +3,26 @@
 #reads from file and passes results to the login_handler
 #returns a list l where l[0] is user entered password and l[1] is an array of features 
 def parse(filename):
-	print "parsing input file"
-	# with open(filename,'r') as f:
-	# 	for line in f:
-	# 		print line
+	feature = False
+	password_features = [] # list of features per entered password
+	features = [] # list of password_features list
+	list_passwords = [] #list of user entered passwords
 
-	user_input_pass = "pass"
-	#list of features to return
-	features = [0,3,4,5,1,9,8]
-	ret_list = [user_input_pass, features]
+	print "parsing input file"
+	with open(filename,'r') as f:
+	 	for line in f:
+	 		if feature == False: #store the user input password
+	 			user_input_pass = line
+	 			list_passwords.append(user_input_pass)
+	 			feature = True
+	 		else:
+	 			input_features = line.split(',') # split the input on comma delimeted
+	 			for l in input_features:
+	 				password_features.append(l)
+	 			features.append(password_features)
+	 			feature = False
+	 		
+
+	
+	ret_list = [list_passwords, features]
 	return ret_list
