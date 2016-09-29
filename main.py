0e10d7b170c0cec57d0ccf37689c83db53ebfc90
wjh <wesjhughes@gmail.com>
2016-09-29 19:25:39 -0400

    Updated with initialize history.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
-import parser,sys
+import parser,sys,init_history
 
 def main():
 	#open the input file for reading
 	user_input = parser.parse(sys.argv[1])
+	history = init_history.initialize_history(user_input)
 	#store the user's input password
 	list_pass = user_input[1]
 	#store the list of features
