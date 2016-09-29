1cfd3d93091b368a56559726df3020b78e64805d
Edmonds <brianedmonds90@gmail.com>
2016-09-29 17:33:32 -0400

    Initial read from file

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print "Hello World!"
+#!/usr/bin/python
+
+def main():
+	#open the input file for reading
+	parser()
+
+
+#reads from file and passes results to the login_handler
+def parser():
+	#do something
+	print "parsing file"
+	with open('input-file.txt','r') as f:
+		for line in f:
+			print line
+
+#takes in password and feature list
+def login_handler(user_password, list_features):
+	print "login handler"
+
+
+
+#Run the main function
+if __name__ == '__main__':
+    main()
