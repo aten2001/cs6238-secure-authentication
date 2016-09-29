94df3b3646162922e85bc17615904b94639d179e
Edmonds <brianedmonds90@gmail.com>
2016-09-29 19:24:19 -0400

    merge + making a harder input file

--- init_history.py
+++ init_history.py
@@ -2,4 +2,4 @@
 def initialize_history(list_of_attempts):
 
     for i in range(5):
-        for j in range(list_of_attempts[i,1])
+        for j in range(list_of_attempts[i,1]):
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import parser,sys
 def main():
 	#open the input file for reading
 	user_input = parser.parse(sys.argv[1])
-	#store the user's input password
+	#store the user's input passwords
 	list_pass = user_input[1]
 	#store the list of features
 	list_features = user_input[0]
