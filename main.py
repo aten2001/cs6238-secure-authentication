c336b7a7c56ea214d0db4917e525929c7354137f
brianedmonds90 <brianedmonds90@gmail.com>
2016-10-04 19:20:42 -0400

    Starting to implement the random number generator

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ def main():
 	# need to choose a hardened password
 	hpwd = math.choose_hpwd()
 
+
+	#need to compute mu and sigma
+	#will return a list of mus per feature
+	mu_list = math.compute_mu_list(history)
+	sigma_list=math.compute_sigma_list(history)
+	#figures out if we are slow or fast and computes the instruction table.
+	instruction_table = math.compute_instruction_table(mu_list,sigma_list)
+	pwd = user_input[0][0]
+	#need to encrypt the instruction table with the user entered pwd
+	encrypted_instruction_table = math.encrypt_instruction_table(instruction_table,pwd)
+
 	encrypted_history_file = compute_history.compute_history(history, hpwd)
 
-
 	#check the rest of the user's inputs and see if he or she logged in correctly
-
 
 
 #Run the main function
--- math.py
+++ math.py
@@ -1,4 +1,7 @@
-
+import random
 #during the intitialization phase of the user's first five inputs, we need to generate a random hpwd
 def choose_hpwd():
-    return 50
+    #choose a q that is 160 bits or smaller randomly
+    q =random.randint(0, 2**160 - 1)
+
+    return q
