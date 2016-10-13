85587a6a312a1e429d8a221a165d10dfbce82c69
brianedmonds90 <brianedmonds90@gmail.com>
2016-10-13 17:24:52 -0400

    Preparing to pull 6 7{ 8

--- myMath.py
+++ myMath.py
@@ -1,10 +1,19 @@
 import random
+
+#takes in a random 160 bit number and returns a matching polynomial
+def find_polynomial(q,num_features):
+	#generate a bunch of x,y pairs
+	return 0;
+
 #during the intitialization phase of the user's first five inputs, we need to generate a random hpwd
 def choose_hpwd():
 	#choose a q that is 160 bits or smaller randomly
 	q =random.randint(0, 2**160 - 1)
 
 	return q
+
+
+
 
 def compute_mu_list(h):
 	return False
