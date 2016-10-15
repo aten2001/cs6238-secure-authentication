4dcfc84784704aa898ac17778a6e55df61b2b8fa
brianedmonds90 <brianedmonds90@gmail.com>
2016-10-15 17:07:08 -0400

    Generating the instruction table

--- myMath.py
+++ myMath.py
@@ -56,7 +56,7 @@ def compute_instruction_table(mu_list,sigma_list,hpwd, m, r ,q,pwd):
 
 	# next generate a table of all correct values of the x,y pairings in a double array for both alpha and beta sides
 	#####padding will also be inserted into our list here as well, so the length of the password is hidden
-	xyPairsList = calculate_XY_pairs(coefficientsList,r,pwd)
+	xyPairsList = calculate_XY_pairs(coefficientsList,pwd,r,q)
 
 	#now we will check the different feature values to determine if the user is fast or slow
 	#if user is neither fast nor slow, we will insert a random value into the list that is not equal to the correct value
@@ -133,9 +133,9 @@ def test_recon():
 	print "worked"
 
 #this function will take in all our coefficients (the polynomial function) and return our XY value pairs
-def calculate_XY_pairs(coefficientsList,pwd,r):
+def calculate_XY_pairs(coefficientsList,pwd,r,q):
 
-	#calculate for teh number of features, then pad the rest of the values to a set number
+	#calculate for the number of features, then pad the rest of the values to a set number
 	num_features = len(coefficientsList)
 	instruction_table=[[0 for x in range(num_features)] for y in range(2)]
 	i = 1
@@ -144,15 +144,16 @@ def calculate_XY_pairs(coefficientsList,pwd,r):
 		beta_x = Pr(2*i+1,r)
 		alpha_y = solveForY(coefficientsList,alpha_x)
 		beta_y = solveForY(coefficientsList,beta_x)
-		instruction_table[0][coefficient] =	alpha_y
-		instruction_table[1][coefficient] = beta_y
+		alpha = alpha_y + G(2*i,r,pwd)%q
+		beta = beta_y + G(2*i+1,r,pwd)%q
+		instruction_table[0][coefficient] =	alpha
+		instruction_table[1][coefficient] = beta
 		i = i+1
 	return instruction_table
 
 
 if __name__ == '__main__':
 
-	print "HELLO"
 	test_recon()
 
 	#encrypt_instruction_table([], 0, 0)
