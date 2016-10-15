d4934ba426ecf7127883548b0aa3a3e283806115
brianedmonds90 <brianedmonds90@gmail.com>
2016-10-15 19:29:10 -0400

    History file

--- compute_history.py
+++ compute_history.py
@@ -5,10 +5,8 @@
 # it will use the input as the key to encrypt the history file
 def compute_history(first_five_logins, key):
     # type: (object, object) -> object
+    print "DO SOMETHING"
 
-    with open('history-file.txt', 'w') as f:
-        for features in first_five_logins:
-            f.write(str(features))
-    print "DO SOMETHING HERE"
+
 
 
