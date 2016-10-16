6b5fb3502fd0515059782dc5a758a427fd9b8d62
brianedmonds90 <brianedmonds90@gmail.com>
2016-10-16 16:59:27 -0400

    Working on the compute history file

--- compute_history.py
+++ compute_history.py
@@ -2,13 +2,25 @@
 import logging
 from Crypto.Cipher import AES
 
+logging.basicConfig(filename='status.log', level=logging.DEBUG)
+
 #This function takes in a feature list and begin to build the file.
 # it will use the input as the key to encrypt the history file
 def compute_history(first_five_logins, key):
     # type: (object, object) -> object
-    encyrpted_history_file = [[0 for x in range(first_five_logins[0])] for y in range(2)]
+    list_features = first_five_logins[0]
+    secret = first_five_logins[1]
     cipher = AES.new(key, AES.MODE_ECB)
-    cipher.encrypt(first_five_logins[1])
+    cipher.encrypt(secret)
+
+def decrypt_history(encrypted_h_file,key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    print "DO SOMETHING"
+
+
+if __name__ == '__main__':
+    logging.debug("INSIDE OF THE COMPUTE HISTORY FILE")
+
 
 
 
