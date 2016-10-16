c447ad7d6dd9f2e599fad753ddd5a562a69f78e5
brianedmonds90 <brianedmonds90@gmail.com>
2016-10-16 16:49:09 -0400

    Preparing to pull and working on encyrpting the history file object.

--- compute_history.py
+++ compute_history.py
@@ -1,11 +1,16 @@
 
 import logging
+from Crypto.Cipher import AES
 
 #This function takes in a feature list and begin to build the file.
 # it will use the input as the key to encrypt the history file
 def compute_history(first_five_logins, key):
     # type: (object, object) -> object
-    logging.info("DO SOMETHING HERE")
+    encyrpted_history_file = [[0 for x in range(first_five_logins[0])] for y in range(2)]
+    cipher = AES.new(key, AES.MODE_ECB)
+    cipher.encrypt(first_five_logins[1])
+
+
 
 
 
